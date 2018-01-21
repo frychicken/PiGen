11d4dd1afcfd6ea173d2098db6fdb1e3e7135260
Bob Dinh <bobdinh139@gmail.com>
2018-01-21 16:56:46 -0500

    Update PiGen.sh

--- bash/alpha/PiGen.sh
+++ bash/alpha/PiGen.sh
@@ -3,60 +3,44 @@
 #thank you for using
 # license @bobdinh139
 
-let n=10
-let r=4
-(( fact1 = $n - 1 ))
-(( fact2 = $n - 2 ))
-(( fact3 = $n - 3 ))
-(( fact4 = $n - 4 ))
-(( fact5 = $n - 5 ))
-(( fact6=$n-6 ))
-(( fact7=$n-7 ))
-(( fact8=$n-8 ))
-(( numer = $n * $fact1 * $fact2 * $fact3 * $fact4 * $fact5 * $fact6 * $fact7 * $fact8 ))
-(( sub=$n-$r ))
-(( demo = $sub * $fact5 * $fact6 * $fact7 * $fact8 ))
-(( per = $numer / $demo ))
-(( by = $per * 4 ))
-(( kiloby = $by / 1000 ))
-(( meby = $kiloby / 1000 ))
-(( giby = $meby / 1000 ))
-(( teby = $giby / 1000 )) 
-(( seco = $per * 1 ))
-(( min = $seco / 60 ))
-(( hour = $min / 60 ))
-(( minlef = $min % 60 ))
-echo PIN generator starting...
-sleep 0.5
-echo you are about to generate: $per numbers 
-echo which is about nearly
-echo $seco :seconds 
-echo $min :minutes
-echo $hour hour\(s\) and $minlef minute\(s\) left
-echo $by :bytes
-echo $kiloby :KB
-echo $meby :MB
-echo $giby :GB
-echo $teby :TB
-con () {
-echo Are you sure you want to continue \(y\/n\)? :
-read input
-if [ $input == 'y' ]
- then 
-echo Doing it...
-return
+
+endl () {
+echo [*] Done!
+}
+ixit() {
+((temp4 = $temp4 + 1))
+if [ $temp4 -lt 9 ]
+then
+startl
 fi
-if [ $input == 'n' ]
- then 
-echo exiting...
-exit
- else 
-echo invail input
-con
+if [ $temp4 == 10 ]
+then
+endl
 fi
 }
-con
-
+exitl () {
+((temp = $temp + 1))
+if [ $temp -lt 9 ]
+then
+bigin
+fi
+if [ $temp == 10 ]
+then
+ixit
+fi
+}
+seven19 () {
+((temp1 = $temp1 + 1))
+if [ $temp1 -lt 9 ]
+then
+begin
+fi
+if [ $temp1 == 10 ]
+then
+exitl
+fi
+}
+main () {
 let temp4=0
 
 startl () {
@@ -102,45 +86,72 @@ done
 }
 }
 }
-seven19 () {
-((temp1 = $temp1 + 1))
-if [ $temp1 -lt 9 ]
-then
-begin
-fi
-if [ $temp1 == 10 ]
-then
-exitl
-fi
 }
 
-exitl () {
-((temp = $temp + 1))
-if [ $temp -lt 9 ]
-then
-bigin
+let n=10
+let r=4
+(( fact1 = $n - 1 ))
+(( fact2 = $n - 2 ))
+(( fact3 = $n - 3 ))
+(( fact4 = $n - 4 ))
+(( fact5 = $n - 5 ))
+(( fact6=$n-6 ))
+(( fact7=$n-7 ))
+(( fact8=$n-8 ))
+(( numer = $n * $fact1 * $fact2 * $fact3 * $fact4 * $fact5 * $fact6 * $fact7 * $fact8 ))
+(( sub=$n-$r ))
+(( demo = $sub * $fact5 * $fact6 * $fact7 * $fact8 ))
+(( per = $numer / $demo ))
+(( by = $per * 4 ))
+(( kiloby = $by / 1000 ))
+(( meby = $kiloby / 1000 ))
+(( giby = $meby / 1000 ))
+(( teby = $giby / 1000 )) 
+(( seco = $per * 1 ))
+(( min = $seco / 60 ))
+(( hour = $min / 60 ))
+(( minlef = $min % 60 ))
+echo PIN generator starting...
+sleep 0.5
+echo you are about to generate: $per numbers 
+echo which is about nearly
+echo $seco :seconds 
+echo $min :minutes
+echo $hour hour\(s\) and $minlef minute\(s\) left
+echo $by :bytes
+echo $kiloby :KB
+echo $meby :MB
+echo $giby :GB
+echo $teby :TB
+con () {
+echo Are you sure you want to continue \(y\/n\)? :
+read input
+if [ $input == 'y' ]
+ then 
+echo Doing it...
+main
+return
 fi
-if [ $temp == 10 ]
-then
-ixit
+if [ $input == 'n' ]
+ then 
+echo exiting...
+exit
+ else 
+echo invail input
+con
 fi
 }
+con
 
-ixit() {
-((temp4 = $temp4 + 1))
-if [ $temp4 -lt 9 ]
-then
-startl
-fi
-if [ $temp4 == 10 ]
-then
-endl
-fi
-}
 
-endl () {
-echo [*] Done!
-}
+
+
+
+
+
+
+
+
 
 
 
